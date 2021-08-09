7b5dc948f016486d25e2010dc36704ebf879b796
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-09 12:21:56 +0530

    UC1 main class

--- Welcome.java
+++ Welcome.java
@@ -1,10 +1,10 @@
 
-public class Welcome {
+public class Welcome extends AdressBookUC1 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welocome To Adress book here you can store your complete address..");
-
+		addContact();
 	}
 
 }
